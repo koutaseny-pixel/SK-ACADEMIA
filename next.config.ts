f03d36273863d0ff3a766160558dc63e1ebc9b6f
SK Academia Dev <dev@skacademia.sn>
2026-08-07 22:32:48 +0000

    chore: update basePath for new repository

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/SK-ACADEMIA-WEB",
+  basePath: "/SK-ACADEMIA",
   images: {
     unoptimized: true,
   },
